1970128a30f6a10e10102243712c27f7edb1d11a
Sequoia Nevae <sequoianevae@Sequoias-iMac.lan>
2024-02-06 19:36:58 -0600

    updated to submit

--- FreelancerForum.js
+++ FreelancerForum.js
@@ -1,6 +1,6 @@
-const names = ["Alice", "Bob"];
-const occupations = ["writer", "teacher"];
-const prices = [30, 50];
+const names = ["Alice", "Bob", "Jerry", "Sarah"];
+const occupations = ["writer", "teacher", "coach", "programmer"];
+const prices = [30, 50, 40, 75];
 const maxFreelancers = 20;
 const freelancers = [
     {
@@ -16,17 +16,17 @@ const freelancers = [
 ];
 
 const addFreelancerIntervalId = setInterval(addFreelancer, 5000);
+
 render();
 
 function render(){
-    const freelancer = document.querySelector("#freelancers");
-    const freelancerElements = freelancers.map((freelancer) => {
+    const freeLancer = document.querySelector("#freelancers");
+    const freeLancerElements = freelancers.map((freelancer) => {
         const element = document.createElement("li");
-        element.classList.add("freelancers", freelancer.names, freelancer.occupations, freelancer.prices);
+        element.classList.add(freelancers.name, freelancers.occupation, freelancers.price);
         return element;
     });
-    freelancer.replaceChildren(...freelancerElements);
-    render();
+    freeLancer.replaceChildren(...freeLancerElements);
 
 };
 
@@ -34,10 +34,22 @@ function addFreelancer() {
     const name = names[Math.floor(Math.random() * names.length)];
     const occupation = occupations[Math.floor(Math.random() * occupations.length)];
     const price = prices[Math.floor(Math.random() * prices.length)];
+    
     freelancers.push({name, occupation, price});
+    
     render();
-
-    if(freelancers.length === maxFreelancers){
-        clearInterval(addFreelancerIntervalId);
+    
     }
+    if(freelancers.length >= maxFreelancers){
+        clearInterval(addFreelancerIntervalId);
 }
+
+function getSum(price) {
+    let sum = 0
+    for(let i = 0; i < price.length; i++){
+      sum = sum += price[i];
+    }
+    return sum;
+  }
+
+function averagePrice()
